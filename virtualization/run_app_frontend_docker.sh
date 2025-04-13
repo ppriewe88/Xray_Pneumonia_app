adc4a32c365c4269126731bf965b4f6af64ccc42
ppriewe88 <ppriewe@gmx.net>
2025-04-13 17:11:00 +0200

    separate backend container build files

--- virtualization/run_app_frontend_docker.sh
+++ virtualization/run_app_frontend_docker.sh
@@ -1,6 +1,7 @@
 ################################# NOTICE ##########################
-# This script starts only the frontend in the docker frontend container!
+# This script starts only the FRONTEND in the docker FRONTEND container!
 ################################# NOTICE ##########################
+# in frontend: start react app in frontend directory
 cd /app/frontend
 # in frontend: start react app
 npm run start -- --host 0.0.0.0 --port 3000
