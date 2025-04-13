263b6f549a5f63fe514ca2cdcc9bfc2285504eed
ppriewe88 <ppriewe@gmx.net>
2025-04-13 08:31:31 +0200

    changed requirements, dockerfile and dockerignore

--- run_app_docker.sh
+++ run_app_docker.sh
@@ -1,5 +1,5 @@
 # in backend: navigate to directory mlruns are stored
-cd backend/src/models/
+cd /app/backend/src/models/
 # in backend: start mlflow server in docker (& tells it to run in the background) 
 mlflow server --host 0.0.0.0 --port 8080 &
 # in backend: navigate to api directory
@@ -7,6 +7,8 @@ cd ../api/
 # in backend: start uvicorn server (api) in docker (& tells it to run in the background)
 uvicorn api_server:app --host 0.0.0.0 --port 8000 &
 # navigate to frontend directory
-cd ../../../frontend
+cd /app/frontend
 # in frontend: start react app
-npm run start --host 0.0.0.0 --port 3000
+npm run start -- --host 0.0.0.0 --port 3000
+# keep processes running
+wait
