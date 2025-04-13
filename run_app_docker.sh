4dfd9161df5763c25788cd4530ecca5dc16de65b
ppriewe88 <ppriewe@gmx.net>
2025-04-13 07:48:15 +0200

    changed docker start script (added frontend)

--- run_app_docker.sh
+++ run_app_docker.sh
@@ -1,8 +1,12 @@
-# navigate to directory mlruns are stored
+# in backend: navigate to directory mlruns are stored
 cd backend/src/models/
-# start mlflow server in docker (& tells it to run in the background) 
+# in backend: start mlflow server in docker (& tells it to run in the background) 
 mlflow server --host 0.0.0.0 --port 8080 &
-# navigate to api directory
+# in backend: navigate to api directory
 cd ../api/
-# run api script in docker
-uvicorn api_server:app --host 0.0.0.0 --port 8000
+# in backend: start uvicorn server (api) in docker (& tells it to run in the background)
+uvicorn api_server:app --host 0.0.0.0 --port 8000 &
+# navigate to frontend directory
+cd ../../../frontend
+# in frontend: start react app
+npm run start --host 0.0.0.0 --port 3000
