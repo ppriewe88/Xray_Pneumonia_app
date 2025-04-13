e8ae1ebde3dc0c3532ddad4de0ed8bd247b1712c
ppriewe88 <ppriewe@gmx.net>
2025-04-13 15:00:04 +0200

    minor changes in big dockerfile. Correcting small dockerfile w.r.t. imports!!

--- run_app_docker.sh
+++ run_app_docker.sh
@@ -3,7 +3,7 @@
 # # in backend: start mlflow server in docker (& tells it to run in the background)
 # mlflow server --host 0.0.0.0 --port 8080 &
 # # in backend: navigate to api directory
-# cd ../api/
+# cd /app/backend/api
 # # in backend: start uvicorn server (api) in docker (& tells it to run in the background)
 # uvicorn api_server:app --host 0.0.0.0 --port 8000 &
 # navigate to frontend directory 
