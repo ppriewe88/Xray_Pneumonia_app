671e5727fbaeafbf59da68aa6f651d5befd00214
ppriewe88 <ppriewe@gmx.net>
2025-04-13 16:41:36 +0200

    frontend container separated

--- virtualization/run_app_frontend_docker.sh
+++ virtualization/run_app_frontend_docker.sh
@@ -1,3 +1,6 @@
+################################# NOTICE ##########################
+# This script starts only the frontend in the docker frontend container!
+################################# NOTICE ##########################
 cd /app/frontend
 # in frontend: start react app
 npm run start -- --host 0.0.0.0 --port 3000
