b8662d110631d979f57094523a3d775d1fc752b0
ppriewe88 <ppriewe@gmx.net>
2025-04-13 14:06:17 +0200

    jo

--- run_app_docker.sh
+++ run_app_docker.sh
@@ -1,6 +1,6 @@
 # # in backend: navigate to directory mlruns are stored
 # cd /app/backend/src/models/
-# # in backend: start mlflow server in docker (& tells it to run in the background) 
+# # in backend: start mlflow server in docker (& tells it to run in the background)
 # mlflow server --host 0.0.0.0 --port 8080 &
 # # in backend: navigate to api directory
 # cd ../api/
